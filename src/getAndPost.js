7930fd9aa0dadf253fbd7da48d21c11b0a44e12d
avinashsaklani <avinashsaklani7@gmail.com>
2023-07-23 00:31:58 +0530

    understanding get and post

--- src/getAndPost.js
+++ src/getAndPost.js
@@ -28,6 +28,13 @@ app.get('/movies', (req, res) => {
     res.send(moviesList)
 })
 
+app.get('/movies/:name', (req, res) => {
+    //route parameters
+    const { name } = req.params
+    const movie = moviesList.find((m) => m.name === name)
+    res.send(movie)
+})
+
 app.post('/movies', (req, res) => {
     console.log(req.body)
     moviesList.push(req.body)
