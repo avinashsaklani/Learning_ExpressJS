7dae1dda41a3cd7b0b292da8714149d45bfa4ade
avinashsaklani <avinashsaklani7@gmail.com>
2023-07-22 17:32:29 +0530

    Basic skeleton of Routing

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+
+const app = express()
+
+let name = 'Avinash'
+let Password = 1234567
+//api methods - get post put delete
+// app.get(route, callback(req, res))
+
+app.get('/', (req, res) => {
+    res.send('This is the Login Page')
+}) // login page
+
+app.use(loginMiddleWare)
+
+app.get('/profilePage', (req, res) => {
+    res.send('Hello! My fullname is Avinash Saklani')
+})
+
+app.get('/feedPage', (req, res) => {
+    res.send('Hello! Here are all your feeds')
+})
+
+app.listen(8000, () => {
+    console.log('Port running on 8000')
+})
+
+
+function loginMiddleWare(req, res, next) {
+    if (name == 'Avinash' && Password == 1234567) {
+        next()
+    }
+    else {
+        res.send('Cannot authenticate the user')
+    }
+}
